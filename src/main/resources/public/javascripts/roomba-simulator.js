8ddd63a287733d9a45eb879c03cc1f3d7bc42725
Rithvik Rao <rithvik321@gmail.com>
2017-08-12 12:40:24 -0700

    CODEMIRROR WORKS

--- src/main/resources/public/javascripts/roomba-simulator.js
+++ src/main/resources/public/javascripts/roomba-simulator.js
@@ -1,5 +1,12 @@
-var roombaSim = angular.module('roombaSimApp', []);
+var roombaSim = angular.module('roombaSimApp', ['ui.codemirror']);
 roombaSim.controller('roombaSimController', function($scope)
 {
-$scope.sayHello = function() {window.alert("Hello") }
+$scope.sayHello = function() {window.alert("Hello") };
+$scope.editorOptions = {
+	lineWrapping: true,
+	lineNumbers: true,
+	matchBrackets: true,
+	mode: 'text/x-java'
+};
+$scope.code = '';
 });
